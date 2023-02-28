18ce84dbdff1d9ddce95262c85e7e8fba1892362
shanelevites <srlevit94@gmail.com>
2023-02-27 17:14:09 -0800

    main index.js made, npm packages installed.

---
+++ routes/api/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
+
+// TODOS:
+// DELETE to remove user by its _id
+
+// BONUS: Remove a user's associated thoughts when deleted.
+
+
+
+module.exports = router;
